8fa4d1ec58ced3f773af8233c2e35ea2b6786d72
coproductions <constantin.dumba@gmail.com>
2024-12-15 17:20:36 +0100

    updates vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/babysittlingcal/', // Replace 'your-repo-name' with your GitHub repository name
+  build: {
+    outDir: 'dist', // Optional: Customize the output directory
+  },
 })
